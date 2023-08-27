a4995c642f53f727922e2f28bb9035422a4c6273
feuvpi <fredvpg@gmail.com>
2023-08-26 22:01:27 -0300

    initial commit, initial config

---
+++ src/main/java/com/picpay/picpaydemo/PicpayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.picpaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpay/picpaydemo/PicpayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.picpaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
