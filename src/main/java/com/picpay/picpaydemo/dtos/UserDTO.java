07399ae4f5b054e1e7ec8b3eb32b31261968e434
feuvpi <fredvpg@gmail.com>
2023-09-03 22:40:56 -0300

    UserDTO

--- src/main/java/com/picpay/picpaydemo/dtos/UserDTO.java
+++ src/main/java/com/picpay/picpaydemo/dtos/UserDTO.java
@@ -1,8 +1,10 @@
 package com.picpay.picpaydemo.dtos;
 
+import com.picpay.picpaydemo.domain.user.UserType;
+
 import java.math.BigDecimal;
 
-public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password) {
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, BigDecimal amount) {
 
 
 }
