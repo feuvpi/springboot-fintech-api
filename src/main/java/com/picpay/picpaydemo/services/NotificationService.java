656ac00ceb1f8a70ae1b309a56ec980fe9852980
feuvpi <fredvpg@gmail.com>
2023-09-07 21:59:22 -0300

    NotificationService sendNotification

--- src/main/java/com/picpay/picpaydemo/services/NotificationService.java
+++ src/main/java/com/picpay/picpaydemo/services/NotificationService.java
@@ -1,5 +1,9 @@
 package com.picpay.picpaydemo.services;
+import com.picpay.picpaydemo.domain.user.User;
+import com.picpay.picpaydemo.dtos.NotificationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -7,9 +11,14 @@ import org.springframework.web.client.RestTemplate;
 public class NotificationService {
     @Autowired
     private RestTemplate restTemplate;
-    public void sendNotification(User user, String message){
+    public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
 
-        = restTemplate.postForEntity();
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("Notification service not available.");
+            throw new Exception("Notification service not available.");
+        }
     }
 }
