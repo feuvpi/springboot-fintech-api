c2e5ee0cf8c0e07ab6f6ad2569ab12c4d8753d28
feuvpi <fredvpg@gmail.com>
2023-09-07 21:59:36 -0300

    ControllerExceptionHandler

---
+++ src/main/java/com/picpay/picpaydemo/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.picpay.picpaydemo.controllers;
+
+import com.picpay.picpaydemo.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("User already registered.", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(EntityNotFoundException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+}
