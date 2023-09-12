602bf70f8cef60712ccce649f091505a20533bef
feuvpi <fredvpg@gmail.com>
2023-09-11 23:53:27 -0300

    Creating class to base Admin and User. Admin and User are going to different tables due to very diverse nature

---
+++ src/main/java/com/picpay/picpaydemo/domain/user/Admin.java
@@ -0,0 +1,15 @@
+package com.picpay.picpaydemo.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="admins")
+@Table(name="admins")
+@Getter
+@Setter
+public class Admin {
+
+
+}
