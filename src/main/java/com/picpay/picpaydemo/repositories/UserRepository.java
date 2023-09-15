69900beb8e348d7775b61ae5fe63407ff07c350a
feuvpi <fredvpg@gmail.com>
2023-09-15 10:49:24 -0300

    findByEmail

--- src/main/java/com/picpay/picpaydemo/repositories/UserRepository.java
+++ src/main/java/com/picpay/picpaydemo/repositories/UserRepository.java
@@ -2,14 +2,13 @@ package com.picpay.picpaydemo.repositories;
 
 import com.picpay.picpaydemo.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
     Optional<User> findUserByDocument(String document);
-
     Optional<User> findUserById(String id);
-
+    UserDetails findByEmail(String email);
 }
