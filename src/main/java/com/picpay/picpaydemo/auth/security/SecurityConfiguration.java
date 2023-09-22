0ef875933b84199677915f356de9a05d815c1e03
feuvpi <fredvpg@gmail.com>
2023-09-21 21:48:01 -0300

    SecurityConfiguration

--- src/main/java/com/picpay/picpaydemo/auth/security/SecurityConfiguration.java
+++ src/main/java/com/picpay/picpaydemo/auth/security/SecurityConfiguration.java
@@ -3,9 +3,13 @@ package com.picpay.picpaydemo.auth.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,6 +25,16 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEnconder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
