fb694914d4aa9c49c94cb836d3b69f91258e9d9c
feuvpi <fredvpg@gmail.com>
2023-09-23 22:03:35 -0300

    AuthController Register

--- src/main/java/com/picpay/picpaydemo/controllers/AuthController.java
+++ src/main/java/com/picpay/picpaydemo/controllers/AuthController.java
@@ -1,10 +1,13 @@
 package com.picpay.picpaydemo.controllers;
-
+import com.picpay.picpaydemo.domain.user.User;
 import com.picpay.picpaydemo.dtos.AuthDTO;
+import com.picpay.picpaydemo.dtos.RegisterDTO;
+import com.picpay.picpaydemo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +20,23 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository repository;
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Validated AuthDTO authDTO){
         var userPassword = new UsernamePasswordAuthenticationToken(authDTO.email(), authDTO.password());
         var auth = this.authenticationManager.authenticate(userPassword);
         return ResponseEntity.ok().build();
 
+    }
+
+    @PostMapping("register")
+    public ResponseEntity register(@RequestBody @Validated RegisterDTO data){
+        if(this.repository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.email(), encryptedPassword, data.firstName(), data.lastName(), data.document());
+        this.repository.save(newUser);
+        return ResponseEntity.ok().build();
     }
 
 
